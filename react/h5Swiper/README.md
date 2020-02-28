6995ec5f21059578a07f0a0825fe1bdaecd66b02
huajing <huajing@microants.cn>
2020-02-28 16:58:28 +0800

    feat:react的h5轮播

---
+++ react/h5Swiper/README.md
@@ -0,0 +1,8 @@
+# 仿淘宝h5商品轮播图
+
+🔖 基于React Hooks写的，需要用到swiper库
+
+props解析：
+
++ video: 视频地址，string
++ images：图片组，array，每项为一个对象`{picurl:'图片地址'}`
---
+++ react/h5Swiper/index.js
@@ -0,0 +1,114 @@
+import React, { useState, useEffect, useCallback, useRef } from "react";
+import Swiper from "swiper";
+import "../../common/swiper.min.css";
+import "./index.css";
+import playerBtn from "./img/playerBtn.png";
+
+const Comp = ({ video = "", images = [] }) => {
+  const [playStatus, setPlayStatus] = useState(false);
+  const slideVideo = useRef(null);
+  const pagination = useRef(null);
+
+  const onVideoPlay = () => {
+    slideVideo.current.play();
+    setPlayStatus(true);
+    return false;
+  };
+
+  const onVideoPause = useCallback(() => {
+    if (slideVideo.current) {
+      slideVideo.current.pause();
+      setPlayStatus(false);
+    }
+    return false;
+  }, [slideVideo]);
+
+  useEffect(() => {
+    if (images.length > 0) {
+      const swiper = new Swiper(".swiper-container", {
+        pagination: {
+          el: ".swiper-pagination",
+          type: "fraction",
+          formatFractionTotal: num => {
+            if (video) {
+              return num - 1;
+            }
+            return num;
+          },
+          formatFractionCurrent: num => {
+            if (video) {
+              return num - 1;
+            }
+            return num;
+          }
+        },
+        paginationClickable: true,
+        updateOnImagesReady: true,
+        lazyLoading: true,
+        lazyLoadingInPrevNext: true,
+        lazyLoadingInPrevNextAmount: 2,
+        on: {
+          slideChangeTransitionStart: () => {
+            onVideoPause();
+            const sliderIndex = swiper.activeIndex;
+            if (sliderIndex == 0 && video) {
+              pagination.current.style.opacity = 0;
+            } else {
+              pagination.current.style.opacity = 1;
+            }
+          }
+        }
+      });
+    }
+  }, [images, video, onVideoPause]);
+
+  return (
+    <div className="carousel">
+      {images.length > 0 ? (
+        <div className="swiper-container swiper">
+          <div className="swiper-wrapper bsd load-div">
+            {video ? (
+              <div className="swiper-slide">
+                <div className="swiper-slideVideo">
+                  <video
+                    src={video}
+                    ref={slideVideo}
+                    poster={images[0].picurl}
+                    width="100%"
+                    height="311"
+                    webkit-playsInline="true"
+                    playsInline="true"
+                    x5-playsInline="true"
+                    // preload=""
+                    x-webkit-airplay="true"
+                    onClick={onVideoPause}
+                  >
+                    <source type="video/mp4" src={video} />
+                  </video>
+                  <div
+                    className={`video_btn ${playStatus ? "videoIsPlay" : ""}`}
+                    onClick={onVideoPlay}
+                  >
+                    <img src={playerBtn} alt="" />
+                  </div>
+                </div>
+              </div>
+            ) : null}
+            {images.map((cur, idx) => {
+              return (
+                <div className="swiper-slide" key={`${cur}${idx + 1}`}>
+                  <img src={cur.picurl} alt="商品图" />
+                </div>
+              );
+            })}
+          </div>
+          <div ref={pagination} style={video ? { opacity: 0 } : {}}>
+            <div className="swiper-pagination"></div>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default Comp;
